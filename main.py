165ca1c7234e8f6a4e28768828dc7c7debfd26f8
Xinyu Li <xinyu.li-20@student.manchester.ac.uk>
2023-11-05 16:31:48 +0000

    feat: Get data from Neo4j database

--- main.py
+++ main.py
@@ -3,4 +3,11 @@ import heapq
 
 graph = Graph("bolt://localhost:7687", auth=("neo4j", "123456789"))
 
+# get all stations and lines
+stations = graph.run("MATCH (s:Station) RETURN s.name AS name").data()
+lines = graph.run("MATCH (l:Line) RETURN l.name AS name, l.cost AS cost").data()
+
+
 # use dijkstra algorithm to find the cheapest way
+def shortest_path():
+    return
