bd16709c9571fc227011d5d4a913e52e172381b3
Xinyu Li <xinyu.li-20@student.manchester.ac.uk>
2023-11-11 00:22:56 +0000

    fix: improved cost calculation in graph-based pathfinding
    - Corrected algorithm to accurately account for line switching costs.
    - Ensured true cost of travel is reflected in pathfinding results.
    - Fixed inaccuracies in cost calculation when transferring between lines.

--- main.py
+++ main.py
@@ -7,31 +7,26 @@ stations = graph.run("MATCH (s:Station) RETURN s.name AS name").data()
 lines = graph.run("MATCH (l:Line) RETURN l.name AS name, l.cost AS cost").data()
 
 
-# use dijkstra algorithm to find the cheapest way
+# use dijkstra algorithm to find the cheapest way with line switching cost
 def cheapest_way(graph_structure, start, end):
-    cheapest_paths = {start: (None, 0)}  # {node: (predecessor, weight)}
+    cheapest_paths = {start: (None, 0, None)}  # {node: (predecessor, weight, line)}
     current_node = start
     visited = set()
 
     while current_node != end:
         visited.add(current_node)
-        destinations = [node for node in graph_structure if node not in visited]
-        weight_to_current_node = cheapest_paths[current_node][1]
-        print("cheapest paths: ", cheapest_paths)
+        destinations = set(graph_structure[current_node]) - visited
+        weight_to_current_node, current_line = cheapest_paths[current_node][1], cheapest_paths[current_node][2]
 
         for next_node in destinations:
-            if next_node in graph_structure[current_node]:  # Check if next_node is a valid destination
-                print("cheapest paths: ", cheapest_paths)
-                print("current node: ", current_node)
-                print("next node: ", next_node)
-                weight = graph_structure[current_node][next_node] + weight_to_current_node
-                print("weight: ", weight)
-                if next_node not in cheapest_paths:
-                    cheapest_paths[next_node] = (current_node, weight)
-                else:
-                    current_shortest_weight = cheapest_paths[next_node][1]
-                    if current_shortest_weight > weight:
-                        cheapest_paths[next_node] = (current_node, weight)
+            for line, cost in graph_structure[current_node][next_node].items():
+                # Add cost only if switching lines
+                new_cost = weight_to_current_node
+                if line != current_line:
+                    new_cost += cost
+
+                if next_node not in cheapest_paths or cheapest_paths[next_node][1] > new_cost:
+                    cheapest_paths[next_node] = (current_node, new_cost, line)
 
         next_destinations = {node: cheapest_paths[node] for node in cheapest_paths if node not in visited}
         if not next_destinations:
@@ -42,12 +37,16 @@ def cheapest_way(graph_structure, start, end):
 
     # get the complete path
     path = []
+    path_info = {}  # This will store the path and the line used for each segment
     while current_node is not None:
         path.append(current_node)
         next_node = cheapest_paths[current_node][0]
+        line_used = cheapest_paths[current_node][2]  # get the line used
+        path_info[current_node] = line_used
         current_node = next_node
     path = path[::-1]
-    return path
+
+    return path, path_info
 
 
 # define the graph based on the data in the database
@@ -75,42 +74,48 @@ for station in stations:
 
 print("graph_data: ", graph_data)
 
-
-def calculate_total_cost(path, graph_structure):
-    """
-    Calculate the total cost based on the path and the pricing rules.
-    Each segment of the journey has a fixed cost regardless of the number of stations.
-    """
-    total_cost = 0
-    if path:
-        # Start with the cost of the first segment
-        total_cost = graph_structure[path[0]].get(path[1], 0)
-        # Add the cost of subsequent segments if there's a transfer
-        for i in range(1, len(path) - 1):
-            print("the station: ", path[i])
-            next_segment_cost = graph_structure[path[i]].get(path[i + 1], 0)
-            # Only add the cost if it's a transfer to a different line (cost changes)
-            if next_segment_cost != total_cost:
-                total_cost += next_segment_cost
-    return total_cost
-
 # # get the start and end station
 # start_station = input("Please type the start station: ")
 # end_station = input("Please type the end station: ")
 
 
 # find the cheapest way
-cheapest_path = cheapest_way(graph_data, "A", "D")
+cheapest_path, path_info = cheapest_way(graph_data, "A", "D")
+
+
+# calculate the total cost and transfer points
+def calculate_total_cost_and_transfers(cheapest_path, path_info, graph_data):
+    total_cost = 0
+    current_line = None
+    transfer_points = []
+
+    for i in range(len(cheapest_path) - 1):
+        start_station = cheapest_path[i]
+        end_station = cheapest_path[i + 1]
+        line_used = path_info[start_station]
+
+        if line_used != current_line:
+            if current_line is not None:  # 判断是否为第一次乘车
+                transfer_points.append(start_station)
+            current_line = line_used
+            cost = graph_data[start_station][end_station][line_used]
+            total_cost += cost
+
+    return total_cost, transfer_points
+
+
+total_cost, transfer_points = calculate_total_cost_and_transfers(cheapest_path, path_info, graph_data)
 
 # print the result
-print("cheapest route：", cheapest_path)
-print("transfer points：", cheapest_path[1:-1] if len(cheapest_path) > 2 else [])
-print(f"price：{sum([graph_data[cheapest_path[i]][cheapest_path[i+1]] for i in range(len(cheapest_path)-1)])}")
+print(f"cheapest route: {cheapest_path}")
+print(f"transfer points: {transfer_points}")
+print(f"price: {total_cost}")
 
-# debugging area
-test_stations = graph.run("MATCH (s:Station) RETURN s.name AS name LIMIT 10").data()
-print("test stations:", test_stations)
-print("test lines:", lines)
-print("test graph: ", graph_data)
-print("test whole path: ",  cheapest_path)
-print("test total cost: ", calculate_total_cost(cheapest_path, graph_data))
+
+# # debugging area
+# test_stations = graph.run("MATCH (s:Station) RETURN s.name AS name LIMIT 10").data()
+# print("test stations:", test_stations)
+# print("test lines:", lines)
+# print("test graph: ", graph_data)
+# print("test whole path: ",  cheapest_path)
+# print("test total cost: ", calculate_total_cost(cheapest_path, graph_data))
