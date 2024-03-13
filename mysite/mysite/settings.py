2a372c32011a286f8ac203d7d1124798d3908ce7
Xinyu Li <xinyu.li-20@student.manchester.ac.uk>
2024-03-13 00:17:49 +0000

    feat: Add favicon

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -74,10 +74,10 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.dummy',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
